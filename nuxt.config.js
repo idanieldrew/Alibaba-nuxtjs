47f114b2855961f972596757c3431e5094aa360c
idanieldrew <danielgia35@gmail.com>
2022-01-08 17:21:07 +0330

    First commit

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  // target: 'static',
+
+  mode: 'universal',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'alibaba',
+    htmlAttrs: {
+      lang: 'fa',
+      dir: 'rtl',
+      class: 'h-full'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'visit planets' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['~/static/style.css'],
+
+  loading:
+  {
+    color: '#047857'
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/axios',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/composition-api/module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    /*// https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',*/
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    // ['cookie-universal-nuxt', {alias: 'cook'}]
+  ],
+/*
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseUrl: 'http://localhost:8000/api'
+  },*/
+
+  router:{
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [
+    ]
+  },
+
+  env: {
+    // baseUrl: process.env.BASE_URL || 'http://localhost:8000/api'
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="landing-page divide-x-0">
+ salam
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'index',
+};
+</script>
