d8a863ef69a848afcc6702c4badb1ca2d1dcaf5c
idanieldrew <danielgia35@gmail.com>
2022-01-17 16:19:39 +0330

    Add homepage 1

---
+++ components/Homepage/Homepage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="homepage container mx-auto rounded-3xl absolute inset-0 flex justify-end flex-col">
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from "./Card";
+
+export default {
+  name: "Homepage",
+  components: {Card}
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="landing-page divide-x-0">
-    <Hero />
+    <div class="relative">
+      <Hero />
+      <Homepage class="absolute"/>
+    </div>
   </div>
 </template>
 
 <script>
+
 import Hero from "../components/Hero/Hero";
+import Homepage from "../components/Homepage/Homepage";
+
 export default {
   name: 'index',
-  components: {Hero}
+  components: {
+    Hero,
+    Homepage
+  }
 };
 </script>
